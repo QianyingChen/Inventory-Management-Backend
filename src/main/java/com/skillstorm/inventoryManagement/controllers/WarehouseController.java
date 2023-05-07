a8ff8affa2a0d5362bcc1f0fca2dbdf2e6f3bd88
Teresa Chen <qianyingteresachen@gmail.com>
2023-05-07 03:52:36 -0400

    add Warehouse Controller,Dto, model, repository and service

---
+++ src/main/java/com/skillstorm/inventoryManagement/controllers/WarehouseController.java
@@ -0,0 +1,42 @@
+package com.skillstorm.inventoryManagement.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillstorm.inventoryManagement.dtos.WarehouseDto;
+import com.skillstorm.inventoryManagement.services.WarehouseService;
+
+@RestController
+@RequestMapping("/warehouses")
+public class WarehouseController {
+	@Autowired
+	private WarehouseService warehouseService;
+	
+    @GetMapping
+	public List<WarehouseDto> findAllWarehouse(String name) {
+		return warehouseService.findAllWarehouse(name);
+	}
+	
+	@GetMapping("/{id}")
+	public WarehouseDto findWarehouseById(@PathVariable long id) {
+		return warehouseService.findWarehouseById(id);
+	}
+		
+	@PostMapping
+	public ResponseEntity<WarehouseDto> createWarehouse(@Valid @RequestBody WarehouseDto warehouseData) {
+		WarehouseDto warehouse = warehouseService.createWarehouse(warehouseData);
+		return new ResponseEntity<>(warehouse, HttpStatus.CREATED);
+	}
+
+}
---
+++ src/main/java/com/skillstorm/inventoryManagement/models/Warehouse.java
@@ -0,0 +1,116 @@
+package com.skillstorm.inventoryManagement.models;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.skillstorm.inventoryManagement.dtos.WarehouseDto;
+
+@Entity
+@Table(name = "warehouses")
+public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "warehouse_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "contact_person")
+    private String contactPerson;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    
+    public Warehouse() {
+		
+	}
+
+	public Warehouse(Long id, String name, String address, String contactPerson, String phoneNumber) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.contactPerson = contactPerson;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContactPerson() {
+		return contactPerson;
+	}
+
+	public void setContactPerson(String contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public WarehouseDto toDto() {
+		return new WarehouseDto (id, name, address, contactPerson, phoneNumber);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Warehouse other = (Warehouse) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Warehouse [id=" + id + ", name=" + name + ", address=" + address + ", contactPerson=" + contactPerson
+				+ ", phoneNumber=" + phoneNumber + "]";
+	}
+    
+    
+    
+}
